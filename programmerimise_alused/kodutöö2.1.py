88130e63a8ea30e331ccbff67a2e0a1179be3bca
jasper-tambur <55434220+jasper-tambur@users.noreply.github.com>
2019-11-04 09:28:45 +0200

    Update kodutöö2.1.py

--- programmerimise_alused/kodutöö2.1.py
+++ programmerimise_alused/kodutöö2.1.py
@@ -1,9 +1,3 @@
-import random
-
-Max1 = 100
-Min1 = 1
-#For random: random.randint(Min1,Max1)
-#For predefined: put the numbers in list1 and remove lines 9 - 11
 list1 = [3, 5, 6, 6, 4]
 
 print("Sisend: ", list1)
