9028ca9a85e988b402ce7902ca28d7069f0b3cd2
jasper-tambur <55434220+jasper-tambur@users.noreply.github.com>
2019-11-04 08:56:04 +0200

    Update kodutöö2.1.py

--- programmerimise_alused/kodutöö2.1.py
+++ programmerimise_alused/kodutöö2.1.py
@@ -4,11 +4,7 @@ Max1 = 100
 Min1 = 1
 #For random: random.randint(Min1,Max1)
 #For predefined: put the numbers in list1 and remove lines 9 - 11
-list1 = []
-Number = 5
-for i in range(1, Number + 1):
-    value = int(input("Sisend %d : " %i))
-    list1.append(value)
+list1 = [3, 5, 6, 6, 4]
 
 print("Sisend: ", list1)
 list1.sort()
